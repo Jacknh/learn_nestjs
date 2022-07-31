bcf6cdb33f8f60b3c48d2c53d64e4c04137c48c6
jack <jacknh418@gmail.com>
2022-07-31 21:23:49 +0800

    update docker compose file

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello Jack!!!';
+    return 'Hello Jack, how is it going?';
   }
 }
